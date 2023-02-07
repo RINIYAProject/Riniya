1374c6b2ebdbc8b790ba5b112ddfb1b8abdc20ba
NebraskyTheWolf <matazox302@gmail.com>
2023-02-07 02:12:37 +0000

    update(): update serverinv command

--- src/abstracts/components/BaseTask.ts
+++ src/abstracts/components/BaseTask.ts
@@ -6,20 +6,21 @@
 /*   By: alle.roy <alle.roy.student@42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 06:25:23 by NebraskyThe       #+#    #+#             */
-/*   Updated: 2023/01/30 19:35:07 by alle.roy         ###   ########.fr       */
+/*   Updated: 2023/02/07 02:12:24 by alle.roy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 import Base from "../Base";
-import cron from "node-cron"
+import { CronJob } from "cron"
 
 export default abstract class BaseTask extends Base {
 
-    public readonly task: cron.ScheduledTask;
+    public readonly task: CronJob;
 
     public constructor(name: string, label: string, time: string, listener: Function) {
         super(name, label, "TASK");
 
-        this.task = cron.schedule(time, () => listener);
+        this.task = new CronJob(time, () => listener)
+        this.task.start()
     }
 }
