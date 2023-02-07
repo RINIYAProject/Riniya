e33aa355b5f79395c07eedf687d83917073fee28
NebraskyTheWolf <matazox302@gmail.com>
2023-02-07 02:16:23 +0000

    update(): update serverinv command

--- src/abstracts/components/BaseTask.ts
+++ src/abstracts/components/BaseTask.ts
@@ -6,7 +6,7 @@
 /*   By: alle.roy <alle.roy.student@42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 06:25:23 by NebraskyThe       #+#    #+#             */
-/*   Updated: 2023/02/07 02:12:24 by alle.roy         ###   ########.fr       */
+/*   Updated: 2023/02/07 02:16:07 by alle.roy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ export default abstract class BaseTask extends Base {
     public constructor(name: string, label: string, time: string, listener: Function) {
         super(name, label, "TASK");
 
-        this.task = new CronJob(time, () => listener)
+        this.task = new CronJob(time, listener.bind(this.instance))
         this.task.start()
     }
 }
