e211f03163f147a05e7350347ff95336ed99c863
NebraskyTheWolf <matazox302@gmail.com>
2023-02-07 02:34:19 +0000

    update(src/): removing GuildInvite task

--- src/components/tasks/TasksManager.ts
+++ src/components/tasks/TasksManager.ts
@@ -2,7 +2,6 @@ import Riniya from "@riniya.ts";
 import BaseTask from "@riniya.ts/components/BaseTask";
 import OptionMap from "@riniya.ts/utils/OptionMap";
 import SessionTask from "./api/SessionTask";
-import GuildInviteCheck from "./push_actions/GuildInviteCheck";
 
 export default class TasksManager {
     private TASKS: OptionMap<string, BaseTask>
@@ -13,7 +12,6 @@ export default class TasksManager {
 
     public registerAll(): void {
         this.register(new SessionTask())
-        this.register(new GuildInviteCheck())
     }
 
     private register(task: BaseTask): void {
