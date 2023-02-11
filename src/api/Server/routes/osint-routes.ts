691cfc2be74b2bdbe87e7cbd1350c639fd20da88
NebraskyTheWolf <matazox302@gmail.com>
2023-02-11 04:47:03 +0000

    update(src/): Update base request system. and adding new commands.

--- src/api/Server/routes/osint-routes.ts
+++ src/api/Server/routes/osint-routes.ts
@@ -6,7 +6,7 @@
 /*   By: alle.roy <alle.roy.student@42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/10 23:21:03 by alle.roy          #+#    #+#             */
-/*   Updated: 2023/02/11 03:31:44 by alle.roy         ###   ########.fr       */
+/*   Updated: 2023/02/11 04:46:44 by alle.roy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,9 @@ import AbstractRoutes from "../AbstractRoutes";
 export default class OsintRoutes extends AbstractRoutes {
     public register() {
         this.router.post('/osint/phone', async (req, res) => {
-            if (req.body.identifier === undefined)
-                return res.status(403).json({
-                    status: false,
-                    error: "The identifier cannot be null."
-                })
-            res.status(403).json({
+            res.status(200).json({
                 status: false,
-                error: "Not Implemented Yet."
+                error: req.body.identifier
             })
         })
     }
